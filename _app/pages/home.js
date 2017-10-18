e021f6fa4aaa1ea9285675fb4c9b1b0f70cd838f
IzumiSy <beetle-noise@gmx.com>
2017-10-18 17:49:38 +0900

    Update home to use emit

--- _app/pages/home.js
+++ _app/pages/home.js
@@ -1,17 +1,17 @@
 const html = require('choo/html')
 
-module.exports = function (state, prev, send) {
+module.exports = function (state, emit) {
   return html`
-    <main>
+    <body>
       <h1>Hello, World!</h1>
       <p>If you are seeing this, then the generator works!</p>
       <h2>Demo</h2>
       <h3>${state.title}</h3>
-      <input type="text" oninput=${update} />
-    </main>
+      <input type="text" oninput=${update} value=${state.title} />
+    </body>
   `
 
   function update (e) {
-    send('update', { value: e.target.value })
+    emit('update', { value: e.target.value })
   }
 }
